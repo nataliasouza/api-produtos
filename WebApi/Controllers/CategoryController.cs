b1f599eb4e36fcc1e3d8e2653d2f16d474ea43eb
Natália Souza <nataliasouza.ti@hotmail.com>
2021-10-09 16:39:46 -0300

    Projeto Base

---
+++ WebApi/Controllers/CategoryController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("categories")]
+    public class CategoryController : ControllerBase
+    {
+        [HttpPost]
+        [Route("")]
+        public Category CreateCategory([FromBody] Category model)
+        {
+            return model;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public string ReadCategory()
+        {
+            return "GET";
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public string ReadCategoryById(int id)
+        {
+            return "GET" + id.ToString();
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        public Category UpdateCategory(int id,[FromBody]Category model)
+        {
+            if(model.Id == id)
+                return model;
+            return null;
+        }
+
+        [HttpDelete]
+        [Route("id:int")]
+        public string DeleteCategory()
+        {
+            return "DELETE";
+        }
+    }
+}
---
+++ WebApi/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+//using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    //[Table("Categoria")]
+    public class Category
+    {
+        [Key]
+        //[Column("Cat_Id")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [MaxLength(60, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        public string Title { get; set; }
+
+    }
+}
