d7d11863016aae35d3145c8185665b5908a41be2
Natália Souza <nataliasouza.ti@hotmail.com>
2021-10-09 20:00:47 -0300

    Data Context

---
+++ WebApi/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace WebApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options)
+            : base(options)
+        { }
+
+        public DbSet<Product>Products { get; set; }
+        public DbSet<Category>Categories { get; set; }
+        public DbSet<User>Users { get; set; }
+
+    }
+}
