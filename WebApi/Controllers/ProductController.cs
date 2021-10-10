146d834745900afbb665829a81b9c216deac931d
Natália Souza <nataliasouza.ti@hotmail.com>
2021-10-09 22:26:03 -0300

    Controller de Produtos

---
+++ WebApi/Controllers/ProductController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Data;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("products")]
+    public class ProductController : ControllerBase
+    {
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Product>> Post(
+            [FromServices] DataContext context,
+            [FromBody]Product model)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Products.Add(model);
+                await context.SaveChangesAsync();
+                return Ok(model);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Product>>>ReadProductsList([FromServices]DataContext context)
+        {
+            var products = await context
+                .Products
+                .Include(x => x.Category)
+                .AsNoTracking()
+                .ToListAsync();
+            return products;
+        }
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Product>> ReadProductsById([FromServices] DataContext context, int id)
+        {
+            var product = await context
+                .Products
+                .Include(x => x.Category)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+            return product;
+        }
+        [HttpGet]
+        [Route("categories/{id:int}")]
+        public async Task<ActionResult<List<Product>>> ReadByCategory([FromServices] DataContext context, int id)
+        {
+            var product = await context
+                .Products
+                .Include(x => x.Category)
+                .AsNoTracking()
+                .Where(x => x.Id == id)
+                .ToListAsync();
+            return product;
+        }
+    }
+}
