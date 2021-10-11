de676ab5e9a6b0447c5c047e9e9b896c56f63fde
Natália Souza <nataliasouza.ti@hotmail.com>
2021-10-10 23:44:45 -0300

    UserController

---
+++ WebApi/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Data;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("users")]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [Route("")]
+        [AllowAnonymous]
+        public async Task<ActionResult<User>> CreatUser(
+            [FromServices] DataContext context,
+            [FromBody] User model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                context.Users.Add(model);
+                await context.SaveChangesAsync();
+                return model;
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possível criar um usuário" });
+            }
+        }
+
+    }
+}
